6b9642d262ae685bc1ecfccf91fa17005596dd9c
aakmaluddin <aakmaluddinn@gmail.com>
2024-12-27 17:23:10 +0800

    Add files via upload

--- src/prediction.py
+++ src/prediction.py
@@ -11,7 +11,20 @@ def predict_weather(model, preprocessor, input_data):
     # Prepare input data as DataFrame
     input_df = pd.DataFrame([input_data])
     
+    # Ensure all necessary columns are present in the input
+    required_columns = preprocessor.transformers_[0][2] + preprocessor.transformers_[1][2]
+    missing_columns = set(required_columns) - set(input_data.keys())
+    
+    if missing_columns:
+        for column in missing_columns:
+            # Provide default values for missing columns
+            if column in preprocessor.transformers_[0][2]:  # Numeric columns
+                input_data[column] = np.nan
+            else:  # Categorical columns
+                input_data[column] = 'missing'
+    
     # Preprocess input
+    input_df = pd.DataFrame([input_data])
     input_preprocessed = preprocessor.transform(input_df)
     
     # Predict
